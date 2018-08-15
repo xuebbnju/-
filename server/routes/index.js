cf7ee540870e2bff92660197438a5a6839a56275
xuebbnju <xue_bb@126.com>
2018-08-15 18:33:04 +0800

    分理出后端代码

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app){
-  app.use('/api/user', require('./user'));
-  app.use('/api/blog', require('./blog'));
-  // app.use('./classify', require('./classify'));
-  app.get('/', function(req, res, next) {
-     res.render('index', { title: 'Express' });
-  });
-};
