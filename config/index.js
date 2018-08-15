9d1e8eb8be342a3fcb2e88badbd7a30b02ae5f99
xuebbnju <xue_bb@126.com>
2018-08-15 11:43:21 +0800

    修改server端口

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = {
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
     proxyTable: {
       '/api':{
-        target:'http://127.0.0.1:4000/api',
+        target:'http://127.0.0.1:3000/api',
         changeOrigin:true,
         pathRewrite:{
           '^/api':''
